d6f3934bad11026bbf85793b01783aa4039725f8
Filippo Albertini <filippo@filippoalbertini.it>
2022-03-08 10:21:20 +0100

    Aggiungere i file di progetto.

---
+++ AS2122_4E_INF_Prof_TestGit/frmMain.cs
@@ -0,0 +1,10 @@
+namespace AS2122_4E_INF_Prof_TestGit
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
